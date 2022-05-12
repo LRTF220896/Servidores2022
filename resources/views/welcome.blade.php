cf099aeea74e6843d10cdee05e0b51afd18f8bb0
LRFT220896 <L17180880@slp.tecnm.mx>
2022-05-12 17:42:36 -0500

    Modificacion 12-05

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,8 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto text-gray-700 sm:h-20">
-                        <h1>Gestion de servidores - Luis Roberto Tavera Flores - 2022</h1>
+                        <h1>Luis Roberto Tavera Flores - 2022</h1>
+                        <h2>Unidad 3 - Gestion de servidores</h2>
                     </svg>
                 </div>
 
